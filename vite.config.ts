d3c330e92e62ab6c84cb81706be5a78bc9e859bd
unknown <zozotech>
2026-04-06 23:54:49 +0600

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-     base: '/zozotech/', // 👈 ADD THIS LINEnpm run build
+     base: '/', // 👈 ADD THIS LINEnpm run build
 
     plugins: [react(), tailwindcss()],
     define: {
